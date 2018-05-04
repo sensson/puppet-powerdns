910397c216543ab9898ac595af57fe232a57e77e
Anthony Somerset <anthony@somersettechsolutions.co.uk>
2018-05-04 15:40:49 +0200

    cleanup spaces/tabs and other build errors

--- manifests/backends/bind.pp
+++ manifests/backends/bind.pp
@@ -22,27 +22,27 @@ class powerdns::backends::bind inherits powerdns {
     require => Package[$::powerdns::params::authoritative_package],
   }
 
-  file { "$::powerdns::params::authoritative_configdirbindbackend/bindbackend.conf": 
+  file { "${::powerdns::params::authoritative_configdirbindbackend}/bindbackend.conf":
     ensure => file,
     mode   => '0644',
     owner  => 'root',
     group  => 'root',
   }
 
-  file { "$::powerdns::params::authoritative_configdirbindbackend/bind":
+  file { "${}::powerdns::params::authoritative_configdirbindbackend}/bind":
     ensure => directory,
     mode   => '0755',
     owner  => 'root',
     group  => 'root',
   }
 
-  file_line { "powerdns-bind-baseconfig":
-    ensure            => present,
-    path              => "$::powerdns::params::authoritative_configdirbindbackend/bindbackend.conf",
-    line              => 'options { directory "/etc/powerdns/bind"; };',
-    match             => 'options',
-    require           => Package['pdns-backend-bind'],
-    notify            => Service[$::powerdns::params::authoritative_service],
+  file_line { 'powerdns-bind-baseconfig':
+    ensure  => present,
+    path    => "${::powerdns::params::authoritative_configdirbindbackend}/bindbackend.conf",
+    line    => 'options { directory "/etc/powerdns/bind"; };',
+    match   => 'options',
+    require => Package['pdns-backend-bind'],
+    notify  => Service[$::powerdns::params::authoritative_service],
   }
 
 }
