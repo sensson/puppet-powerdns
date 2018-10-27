52f075e47e9d5243b6097a08ca916d18c37267c2
ju5t <m@ju5t.nl>
2018-10-27 21:58:40 +0200

    (#58) Correct rubocop trailing comma

--- spec/classes/powerdns_init_spec.rb
+++ spec/classes/powerdns_init_spec.rb
@@ -118,7 +118,7 @@ describe 'powerdns', type: :class do
               db_root_password: 'foobar',
               db_username: 'foo',
               db_password: 'bar',
-              custom_epel: true,
+              custom_epel: true
             }
           end
 
