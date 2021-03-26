ddc8c5a6fc54f145ffa35042a461354aa66e4cb2
Timo Bergemann <extern.timo.bergemann@4com.de>
2021-03-26 12:19:23 +0100

    default path to sqlite scheme went more generic #03

--- manifests/params.pp
+++ manifests/params.pp
@@ -35,7 +35,20 @@ class powerdns::params {
       $sqlite_backend_package_name = 'pdns-backend-sqlite'
       $mysql_schema_file = '/usr/share/doc/pdns-backend-mysql-4.?.?/schema.mysql.sql'
       $pgsql_schema_file = '/usr/share/doc/pdns-backend-postgresql-4.?.?/schema.pgsql.sql'
-      $sqlite_schema_file = '/usr/share/doc/pdns-backend-sqlite*/schema.sqlite3.sql'
+      case $facts['os']['release']['major'] {
+          '5': {
+      	      $sqlite_schema_file = '/usr/share/doc/pdns-backend-sqlite-4.?.?/schema.sqlite3.sql'
+	   }
+          '6': {
+      	      $sqlite_schema_file = '/usr/share/doc/pdns-backend-sqlite-4.?.?/schema.sqlite3.sql'
+	   }
+          '7': {
+      	      $sqlite_schema_file = '/usr/share/doc/pdns-backend-sqlite-4.?.?/schema.sqlite3.sql'
+	   }
+	   default : {
+      	      $sqlite_schema_file = '/usr/share/doc/pdns-backend-sqlite/schema.sqlite3.sql'
+	   }
+      }
       $sqlite_package_name = 'sqlite'
       $authoritative_configdir = '/etc/pdns'
       $recursor_package = 'pdns-recursor'
